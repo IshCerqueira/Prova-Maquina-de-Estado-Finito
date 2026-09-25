908cdd6ab48bbd85b70225400d107d9aea37dcf9
Felipe Cerqueira Santos <felipecerqueirasantos123@gmail.com>
2026-09-25 03:49:30 -0300

    Novas animacoes adicionadas

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class PlayerMovementScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     // Inicialização das variaveis
     private float moveSpeed = 1f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Vector2 lastMoveDirection;
+    private Vector2 ZeroSpeed;
     private Animator animator;
-
-
+    private bool canWalk = true;
+    private bool crouching = false;
+    private bool alive = true;
 
 
     // Puxa o rigidbody do objeto jogador dentro da unity
@@ -23,8 +25,9 @@ public class PlayerMovementScript : MonoBehaviour
     // altera o vetor de movimento em relação a direção apertada e a velocidade pre determinada
     void Update()
     {
-         
-        rb.linearVelocity = moveInput * moveSpeed;
+         if(canWalk && alive){
+            rb.linearVelocity = moveInput * moveSpeed;
+         }
         
     }
 
@@ -35,7 +38,7 @@ public class PlayerMovementScript : MonoBehaviour
         animator.SetBool("IsWalking", true);
 
         // averigua se o jogador parou de andar e na sequencia salva seu ultimo input apertado e passa para a rotação da mira
-        if (context.canceled)
+        if (context.canceled && alive)
         {
             animator.SetBool("IsWalking", false);
             animator.SetFloat("LastX", moveInput.x);
@@ -54,8 +57,86 @@ public class PlayerMovementScript : MonoBehaviour
     }
 
     public void Attack1(InputAction.CallbackContext context){
-        if(context.performed && moveInput.x == 0 && moveInput.y == 0){
+        if(context.performed && moveInput.x == 0 && moveInput.y == 0 && !crouching){
              animator.SetTrigger("Attack1");
         }
+
+        else if(context.performed && (moveInput.x != 0 || moveInput.y != 0) && !crouching){
+            animator.SetTrigger("AttackRun");
+        }
     }
+
+    public void Attack2(InputAction.CallbackContext context){
+       if(context.performed && moveInput.x == 0 && moveInput.y == 0 && !crouching){
+             animator.SetTrigger("Attack2");
+        }
+
+        else if(context.performed && (moveInput.x != 0 || moveInput.y != 0) && !crouching){
+            animator.SetTrigger("AttackRun");
+        }
+    }
+
+     public void Attack3(InputAction.CallbackContext context){
+       if(context.performed && moveInput.x == 0 && moveInput.y == 0 && !crouching){
+             animator.SetTrigger("Attack3");
+        }
+
+        else if(context.performed && (moveInput.x != 0 || moveInput.y != 0) && !crouching){
+            animator.SetTrigger("AttackRun");
+        }
+    }
+
+
+    public void Block(InputAction.CallbackContext context){
+       if(context.performed && moveInput.x == 0 && moveInput.y == 0){
+             animator.SetBool("IsBlocking", true);
+             canWalk = false;
+        }
+        else if(context.canceled){
+            animator.SetBool("IsBlocking", false);
+            canWalk = true;
+        }
+    }
+
+    public void CastSpell(InputAction.CallbackContext context){
+       if(context.performed && moveInput.x == 0 && moveInput.y == 0 && !crouching){
+             animator.SetTrigger("CastSpell");
+              
+        }
+        else if(context.performed && (moveInput.x != 0 || moveInput.y != 0) && !crouching){
+            animator.SetFloat("LastX", moveInput.x);
+            animator.SetFloat("LastY", moveInput.y);
+            animator.SetTrigger("CastSpell");
+        }
+    }
+
+     public void Crouch(InputAction.CallbackContext context){
+       if(context.performed && moveInput.x == 0 && moveInput.y == 0){
+             
+             crouching = !crouching;
+             animator.SetBool("IsCrouch", crouching);
+              
+        }
+        else if(context.performed && (moveInput.x != 0 || moveInput.y != 0)){
+            animator.SetFloat("LastX", moveInput.x);
+            animator.SetFloat("LastY", moveInput.y);
+
+            crouching = !crouching;
+             animator.SetBool("IsCrouch", crouching);
+        }
+    }
+
+  
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        animator.SetFloat("LastX", moveInput.x);
+        animator.SetFloat("LastY", moveInput.y);
+        rb.linearVelocity = ZeroSpeed;
+       alive = false;
+       animator.SetBool("IsDead", true); 
+    }
+
+
+
 }
